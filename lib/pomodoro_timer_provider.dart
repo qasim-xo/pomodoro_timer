37080cc423a0ba8942c78b922ab1ac9cdf42e6a1
qasim-xo <143028673+qasim-xo@users.noreply.github.com>
2025-03-18 21:50:36 +0500

    working on background

--- lib/pomodoro_timer_provider.dart
+++ lib/pomodoro_timer_provider.dart
@@ -4,30 +4,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pomodoro_timer/pomodoro_settings_provider.dart';
 
-
 class PomodoroTimerState {
   bool isRunning;
   bool isBreak;
+  DateTime pausedTime;
   Timer? timer;
   Duration? time;
 
   PomodoroTimerState(
       {required this.isRunning,
+      required this.pausedTime,
       required this.timer,
       required this.time,
       required this.isBreak});
 
   PomodoroTimerState copyWith(
-      {bool? isRunning, Timer? timer, Duration? time, bool? isBreak}) {
+      {bool? isRunning,
+      Timer? timer,
+      Duration? time,
+      bool? isBreak,
+      DateTime? pausedTime}) {
     return PomodoroTimerState(
         isRunning: isRunning ?? this.isRunning,
         timer: timer ?? this.timer,
         time: time ?? this.time,
-        isBreak: isBreak ?? this.isBreak);
+        isBreak: isBreak ?? this.isBreak,
+        pausedTime: pausedTime ?? this.pausedTime);
   }
 
   factory PomodoroTimerState.initial() {
     return PomodoroTimerState(
+        pausedTime: DateTime.now(),
         isRunning: false,
         timer: null,
         time: const Duration(minutes: 25, seconds: 0),
@@ -77,13 +84,32 @@ class PomodoroTimerNotifier extends Notifier<PomodoroTimerState> {
     });
   }
 
+  void resume() {
+    if (remainingSeconds() > 0) {
+      state = state.copyWith(time: Duration(seconds: remainingSeconds()));
+      startTimer();
+    }
+  }
+
+  void setPausedTime(int inSeconds) {
+    final dateTimeNow = DateTime.now();
+    state = state.copyWith(
+        pausedTime: dateTimeNow.add(Duration(seconds: inSeconds)));
+  }
+
+  int remainingSeconds() {
+    final dateTimeNow = DateTime.now();
+    final remainingTime = state.pausedTime.difference(dateTimeNow);
+    return remainingTime.inSeconds;
+  }
+
   void pauseTimer() {
     state = state.copyWith(isRunning: false);
+    setPausedTime(state.time!.inSeconds);
     state.timer!.cancel();
   }
 }
 
 final pomodoroTimerProvider =
     NotifierProvider<PomodoroTimerNotifier, PomodoroTimerState>(
-   PomodoroTimerNotifier.new
-);
+        PomodoroTimerNotifier.new);
